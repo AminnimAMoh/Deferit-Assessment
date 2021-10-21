418f9ef4dc4bc00f1011e68b46c1e69d935d2d45
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-21 21:33:48 +1100

    Just cleaned up.
    Cleaned up the default materials created with create-next.

---
+++ pages/RecieptsList/index.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+function RecieptsList({}: Props): ReactElement {
+    return (
+        <div>
+            <h1>hello</h1>
+        </div>
+    )
+}
+
+export default RecieptsList
