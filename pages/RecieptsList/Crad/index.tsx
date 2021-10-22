e00448e55aed722941a873abacad43c2e07e3f8a
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-22 21:08:37 +1100

    Animation applied to the status and the cards.
    I decided to change the animation from width size to opacity as on width changing was changing the grid layout and it was so confusing.
    However, the way the description cards appear now is more clear and not distracting.
    Also, I highlighted the card hovered on by adding a box shadow to the card.

--- pages/RecieptsList/Crad/index.tsx
+++ pages/RecieptsList/Crad/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Data } from "../../../types/GlobalType";
 import styles from "../../../styles/Card.module.scss";
 import useHover from "../../../hooks/useHover";
+import Snackbar from "../../Shared-Components/Snackbar";
 
 function Card({
   img: { thumbnail, url },
@@ -12,29 +13,12 @@ function Card({
 }: Data): ReactElement {
   const [hoverRef, hoverValue] = useHover<HTMLDivElement>();
   return (
-    <div className={styles.container} ref={hoverRef}>
+    <div ref={hoverRef} className={styles[hoverValue ?'container__open' : 'container__close']}>
       <Image src={thumbnail} alt="content" width="100" height="100" />
       <p>{amount}</p>
       <p>{date}</p>
       <p className={styles.container__status}>{status}</p>
-      {hoverValue && (
-        <div
-          className={styles.container__snack}
-          style={hoverValue ? { width: "auto" } : { width: 0 }}
-        >
-          <h4>Status: {status}</h4>
-          <p
-            className={styles.container__status}
-            style={hoverValue ? { width: "fit-content" } : { width: 0 }}
-          >
-            {status === "processing"
-              ? "This bill is currently in processing, it can take approx. 1-2 hours depending on the time of day."
-              : status === "scheduled"
-              ? "This bill is scheduled to be paid and will be paid on the due date, you are in good hands."
-              : "This is a dummy text. Need to fill in."}
-          </p>
-        </div>
-      )}
+      <Snackbar hoverValue={hoverValue} status={status} />
     </div>
   );
 }
---
+++ pages/Shared-Components/Snackbar.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import styles from "../../styles/SnackBar.module.scss";
+
+interface Props {
+  hoverValue: boolean;
+  status: string;
+}
+
+function Snackbar({ hoverValue, status }: Props): ReactElement {
+  return (
+    <div
+      className={styles[hoverValue ? 'container__open' : 'container__close']}
+    >
+      <h4
+        className={styles.container__status}
+      >
+        Status: {status}
+      </h4>
+      <p
+        className={styles.container__status}
+      >
+        {status === "processing"
+          ? "This bill is currently in processing, it can take approx. 1-2 hours depending on the time of day."
+          : status === "scheduled"
+          ? "This bill is scheduled to be paid and will be paid on the due date, you are in good hands."
+          : "This is a dummy text. Need to fill in."}
+      </p>
+    </div>
+  );
+}
+
+export default Snackbar;
