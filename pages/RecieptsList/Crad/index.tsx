1ddc4a53e093048aff8c7bd7a0ce8dad53cdbc97
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-22 19:38:34 +1100

    Status snack bar.
    Status snack bar is added wondering if I would like to add transitions or not?

--- pages/RecieptsList/Crad/index.tsx
+++ pages/RecieptsList/Crad/index.tsx
@@ -13,10 +13,28 @@ function Card({
   const [hoverRef, hoverValue] = useHover<HTMLDivElement>();
   return (
     <div className={styles.container} ref={hoverRef}>
-        <Image src={thumbnail} alt="content" width="100" height="100" />
-        <p>{amount}</p>
-        <p>{date}</p>
-        <p className={styles.container__status}>{status}</p>
+      <Image src={thumbnail} alt="content" width="100" height="100" />
+      <p>{amount}</p>
+      <p>{date}</p>
+      <p className={styles.container__status}>{status}</p>
+      {hoverValue && (
+        <div
+          className={styles.container__snack}
+          style={hoverValue ? { width: "auto" } : { width: 0 }}
+        >
+          <h4>Status: {status}</h4>
+          <p
+            className={styles.container__status}
+            style={hoverValue ? { width: "fit-content" } : { width: 0 }}
+          >
+            {status === "processing"
+              ? "This bill is currently in processing, it can take approx. 1-2 hours depending on the time of day."
+              : status === "scheduled"
+              ? "This bill is scheduled to be paid and will be paid on the due date, you are in good hands."
+              : "This is a dummy text. Need to fill in."}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
