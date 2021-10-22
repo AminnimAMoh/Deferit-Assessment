01fa15d830713f1d3dc9135176f8405b5db70e57
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-22 19:02:47 +1100

    useHover in place.
    Not tested yet. I have to first split each row into two columns.

--- pages/RecieptsList/Crad/index.tsx
+++ pages/RecieptsList/Crad/index.tsx
@@ -1,17 +1,24 @@
-import React, { ReactElement } from 'react'
-import Image from 'next/image';
-import {Data} from '../../../types/GlobalType';
-import styles from '../../../styles/Card.module.scss'
+import React, { ReactElement } from "react";
+import Image from "next/image";
+import { Data } from "../../../types/GlobalType";
+import styles from "../../../styles/Card.module.scss";
+import useHover from "../../../hooks/useHover";
 
-function Card({img: {thumbnail, url}, amount, date, status}: Data): ReactElement {
-    return (
-        <div className={styles.container}>
-            <Image src={thumbnail} alt='content' width='100' height='100' />
-            <p>{amount}</p>
-            <p>{date}</p>
-            <p className={styles.container__status}>{status}</p>
-        </div>
-    )
+function Card({
+  img: { thumbnail, url },
+  amount,
+  date,
+  status,
+}: Data): ReactElement {
+  const [hoverRef, hoverValue] = useHover<HTMLDivElement>();
+  return (
+    <div className={styles.container} ref={hoverRef}>
+        <Image src={thumbnail} alt="content" width="100" height="100" />
+        <p>{amount}</p>
+        <p>{date}</p>
+        <p className={styles.container__status}>{status}</p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
