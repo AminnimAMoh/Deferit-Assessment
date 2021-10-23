a4f323f946a45f7af2328078d5f0fd84fd07222d
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-23 12:04:26 +1100

    Start implimenting the pagination to the client side.

---
+++ hooks/usePagination.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement, useState, useEffect } from 'react'
+
+interface Props {
+    
+}
+
+function usePagination({}: Props): ReactElement {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default usePagination
